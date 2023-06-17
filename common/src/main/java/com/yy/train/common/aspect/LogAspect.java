fbc811764e68ceb6f2a640b833f6897465860f18
yy <56753470+Novak-joker@users.noreply.github.com>
2023-06-17 23:48:26 +0800

    4.8 增加公共模块，用于放置公共类，如工具类，拦截器，AOP，常量，枚举，公共配置等

--- common/src/main/java/com/yy/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/yy/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.yy.train.member.aspect;
+package com.yy.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -25,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("Common LogAspect");
+        System.out.println("In Common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
