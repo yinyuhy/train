f0d0a91af263801eeadf3611c5ebd0084fb90976
yy <56753470+Novak-joker@users.noreply.github.com>
2023-06-17 21:40:15 +0800

    4.3 新增member会员模块

--- member/src/main/java/com/yy/train/member/controller/TestController.java
+++ member/src/main/java/com/yy/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.yy.train;
+package com.yy.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
