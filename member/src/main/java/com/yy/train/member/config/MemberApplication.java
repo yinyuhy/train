8af819a02f10dd6c7492d1a34f11c464ebb3fba6
yy <56753470+Novak-joker@users.noreply.github.com>
2023-06-22 23:02:49 +0800

    4.13 集成myMabtis，访问数据库成功

--- member/src/main/java/com/yy/train/member/config/MemberApplication.java
+++ member/src/main/java/com/yy/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.yy.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.yy")
+@MapperScan("com.yy.train.member.mapper")
 public class MemberApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
---
+++ member/src/main/java/com/yy/train/member/mapper/MemberMapper.java
@@ -0,0 +1,6 @@
+package com.yy.train.member.mapper;
+
+
+public interface MemberMapper {
+    int count();
+}
