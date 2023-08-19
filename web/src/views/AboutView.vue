f081350c3123b6ff4dc21ad95bbd8b9bc955dac4
yy <56753470+Novak-joker@users.noreply.github.com>
2023-08-19 17:25:58 +0800

    5.4 集成ant design vue及其图标

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button</a-button><plus-circle-outlined />
 
   </div>
 </template>
