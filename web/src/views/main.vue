8bddfa9605e635a0dc18d4165bca6d519d7e606a
yy <56753470+Novak-joker@users.noreply.github.com>
2023-09-05 17:47:08 +0800

    6.10 增加路由登录拦截，访问所有的控制台页面都需要登录

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -23,8 +23,6 @@
 import {defineComponent, ref} from 'vue';
 import TheHeaderView from "@/components/the-header";
 import TheSiderView from "@/components/the-sider";
-import axios from "axios";
-import {notification} from "ant-design-vue";
 
 export default defineComponent({
   name: "login-view",
@@ -32,14 +30,14 @@ export default defineComponent({
 
   setup() {
     const count = ref(0);
-    axios.get("/member/member/count").then((response) => {
-      let data = response.data;
-      if (data.success) {
-        count.value = data.content;
-      } else {
-        notification.error({description: data.message});
-      }
-    });
+    // axios.get("/member/member/count").then((response) => {
+    //   let data = response.data;
+    //   if (data.success) {
+    //     count.value = data.content;
+    //   } else {
+    //     notification.error({description: data.message});
+    //   }
+    // });
     return {
       count
     };
