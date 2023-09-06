b3af34968b9d979ff421635d792e415b4a5f3c55
yy <56753470+Novak-joker@users.noreply.github.com>
2023-09-06 21:23:52 +0800

    7.5 使用线程本地变量存储会员信息，增加拦截器Interceptor生成日志流水号

--- member/src/main/java/com/yy/train/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/yy/train/member/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.yy.train.member.config;
 
+import com.yy.train.common.interceptor.LogInterceptor;
 import com.yy.train.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
@@ -8,18 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LogInterceptor logInterceptor;
 
-   @Resource
-   MemberInterceptor memberInterceptor;
+    @Resource
+    MemberInterceptor memberInterceptor;
 
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-       registry.addInterceptor(memberInterceptor)
-               .addPathPatterns("/**")
-               .excludePathPatterns(
-                       "/member/hello",
-                       "/member/member/send-code",
-                       "/member/member/login"
-               );
-   }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+
+        registry.addInterceptor(memberInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/member/hello",
+                        "/member/member/send-code",
+                        "/member/member/login"
+                );
+    }
 }
