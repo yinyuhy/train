62dd3f6c649c7a0c02c174316be71709b7d02657
yy <56753470+Novak-joker@users.noreply.github.com>
2023-09-06 21:40:41 +0800

    7.6 二级路由制作欢迎页面

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -5,15 +5,10 @@
     <a-layout>
       <the-sider-view></the-sider-view>
       <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          所有会员总数：{{count}}
+          <router-view></router-view>
         </a-layout-content>
       </a-layout>
     </a-layout>
