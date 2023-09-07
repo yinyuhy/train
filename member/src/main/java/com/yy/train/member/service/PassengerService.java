6ff65945587089b1fb87155c6fb0c443838678a6
yy <56753470+Novak-joker@users.noreply.github.com>
2023-09-07 12:58:59 +0800

    7.12 乘车人编辑接口开发

--- member/src/main/java/com/yy/train/member/service/PassengerService.java
+++ member/src/main/java/com/yy/train/member/service/PassengerService.java
@@ -32,11 +32,17 @@ public class PassengerService {
     public void save(PassengerSaveReq passengerReq) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(passengerReq, Passenger.class);
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setMemberId(LoginMemberContext.getId());
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
-        passengerMapper.insert(passenger);
+        if (ObjUtil.isNull(passengerReq.getId())) {
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        }else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+
     }
 
     public PageResp<PassengerQueryResp> queryList(PassengerQueryReq passengerQueryReq) {
