8e769eb0012c13243879ca3870816c7e7f06a5bf
yy <56753470+Novak-joker@users.noreply.github.com>
2023-10-01 11:14:22 +0800

    10.7 增加quartz相关的12张表，增加接口操作quartz

--- batch/src/main/java/com/yy/train/batch/job/TestJob.java
+++ batch/src/main/java/com/yy/train/batch/job/TestJob.java
@@ -1,14 +1,23 @@
 package com.yy.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+//禁止任务并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("quartz job running");
+        System.out.println("quartz job 开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("quartz job 结束");
     }
 }
